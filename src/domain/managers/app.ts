35eef7d64a95714649a1c5fa2fc61476a253bf4f
teawithsand <teawithsand@gmail.com>
2022-11-09 13:14:07 +0000

    Added player to app

--- src/domain/managers/app.ts
+++ src/domain/managers/app.ts
@@ -1,5 +1,16 @@
-import { WhatToPlayManager } from "@app/domain/managers/whatToPlayManager"
+import { FileEntry } from "@app/domain/defines/abookFile"
+import { FileEntryPlayerSourceResolver } from "@app/domain/managers/resolver"
+import {
+	WhatToPlayDataType,
+	WhatToPlayManager,
+} from "@app/domain/managers/whatToPlayManager"
 import { AbookDb } from "@app/domain/storage/db"
+import { MapPlayerSourceProvider, Player } from "@teawithsand/tws-player"
+
+const emptySourceProvider = new MapPlayerSourceProvider<FileEntry>(
+	[],
+	(s) => s.id
+)
 
 export class AppManager {
 	/**
@@ -10,8 +21,24 @@ export class AppManager {
 
 	public readonly whatToPlayManager = new WhatToPlayManager()
 	public readonly abookDb: AbookDb = new AbookDb()
+	public readonly player = new Player(
+		new MapPlayerSourceProvider<FileEntry>([], (s) => s.id),
+		new FileEntryPlayerSourceResolver(this.abookDb)
+	)
 
-	private constructor() {}
+	private constructor() {
+		this.whatToPlayManager.bus.addSubscriber((data) => {
+			if (!data) {
+				this.player.setSourceProvider(emptySourceProvider)
+			} else if (data.type === WhatToPlayDataType.ABOOK) {
+				this.player.setSourceProvider(
+					new MapPlayerSourceProvider(data.abook.entries, (s) => s.id)
+				)
+			} else if (data.type === WhatToPlayDataType.USER_PROVIDED_ENTRIES) {
+				throw new Error("NIY")
+			}
+		})
+	}
 }
 
 export const useAppManager = () => AppManager.instance
