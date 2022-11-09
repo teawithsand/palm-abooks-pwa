97d981671ef2d800a5c6acd247164069acb3ca5c
teawithsand <teawithsand@gmail.com>
2022-11-09 09:24:30 +0000

    Fixed disposition for images

--- src/domain/storage/disposition.ts
+++ src/domain/storage/disposition.ts
@@ -9,9 +9,13 @@ export const guessDisposition = (data: {
 	// 1. Guess using mime
 	if (/^audio\/.+$/.test(mime)) return FileEntryDisposition.MUSIC
 
+	if (/^image\/.+$/.test(mime)) return FileEntryDisposition.IMAGE
+
 	// 2. guess using file name(extension)
 	if (/\.(mp3|ogg|wav|m4a|aac|flac|m4a)$/.test(name))
 		return FileEntryDisposition.MUSIC
+	if (/\.(jpg|jpeg|png|gif|webp)$/.test(name))
+		return FileEntryDisposition.IMAGE
 
 	// 3. Just give up
 	return FileEntryDisposition.UNKNOWN
