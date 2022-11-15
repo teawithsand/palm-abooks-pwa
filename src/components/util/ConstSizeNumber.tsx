5fd5bfe9ee4e25480aa7d0cdf8d5fbc308a41368
teawithsand <teawithsand@gmail.com>
2022-11-15 19:22:10 +0000

    Added constSizeNumber util

---
+++ src/components/util/ConstSizeNumber.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import styled from "styled-components"
+
+const InvisibleInline = styled.span`
+	display: inline;
+	visibility: hidden;
+	opacity: 0;
+`
+
+/**
+ * Util, which creates number, which has constant size.
+ */
+export const ConstSizeNumber = (
+	props: {
+		n: number
+	} & (
+		| {
+				maxNumber: number
+				targetLength?: undefined
+		  }
+		| {
+				maxNumber?: undefined
+				targetLength: number
+		  }
+	)
+) => {
+	const targetLength = props.targetLength ?? props.maxNumber.toString().length
+	// Hack: calculate padding for these entries, so that we
+	// do not have to use grid or table to have numbers in list aligned
+	const padding = "0".repeat(
+		targetLength.toString().length - props.n.toString().length
+	)
+
+	return (
+		<>
+			{props.n}
+			<InvisibleInline>{padding}</InvisibleInline>
+		</>
+	)
+}
