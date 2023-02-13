f42be4b3b62c0056bc68fc568a64aba22382aab2
teawithsand <teawithsand@gmail.com>
2023-02-13 17:08:36 +0000

    Removed no-zero-entries requirement from senderConnOpener

--- src/components/filetransfer/sender/SenderConnOpener.tsx
+++ src/components/filetransfer/sender/SenderConnOpener.tsx
@@ -26,7 +26,7 @@ export const SenderConnOpener = () => {
 	return (
 		<ConnOpener
 			disabled={
-				!peerState.isReady || entries.length === 0
+				!peerState.isReady
 			}
 			token={token}
 			onToken={async (token) => {
