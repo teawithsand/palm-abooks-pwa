e358b7e4062deca6f510efb3248ec32d1a7dfd9c
teawithsand <teawithsand@gmail.com>
2023-02-15 21:35:27 +0000

    Improved version page, so there is more info there

--- src/pages/version.tsx
+++ src/pages/version.tsx
@@ -17,25 +17,49 @@ const Version = styled.h3`
 	text-align: center;
 	font-size: 1.25em;
 `
-const Author = styled.div``
+const Author = styled.div`
+	margin-left: auto;
+	margin-right: auto;
+	text-align: center;
+	font-size: 2.5em;
+	font-size: 1.25em;
+`
 
 const AppInfoPage = () => {
 	const info = useAppTranslationSelector((trans) => trans.info)
 	return (
 		<PageContainer>
 			<Title>PalmABooks PWA</Title>
-			<Version>0.0.1-alpha</Version>
-			<hr />
+			<Version>Version: 0.0.1-alpha</Version>
 			<Author>
+				By <a href={info.autorPageUrl}>Teawithsand</a>
+			</Author>
+			<hr />
+			<div>
+				<h3>Bug reports</h3>
+				<p>
+					In order to report a bug contact me through any channel:{" "}
+					<a href={info.authorContactUrl}>{info.authorContactUrl}</a>
+				</p>
+			</div>
+			<div>
+				<h3>About me & project</h3>
 				<p>
 					Long story short, they call me Teawithsand. I have a blog at{" "}
 					<a href={info.autorPageUrl}>teawithsand.com</a>.
 				</p>
 				<p>
 					For contact info go to{" "}
-					<a href={info.authorContactUrl}>https://teawithsand.com/contact</a>.
+					<a href={info.authorContactUrl}>
+						https://teawithsand.com/contact
+					</a>
+					.
 				</p>
-			</Author>
+				<p>
+					Right now this app is free and ad free, so please enjoy
+					using it.
+				</p>
+			</div>
 		</PageContainer>
 	)
 }
