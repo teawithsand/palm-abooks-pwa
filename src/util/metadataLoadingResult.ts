7868aab7113226a08498ebfda7393f6a81dc20d9
teawithsand <teawithsand@gmail.com>
2023-02-17 21:47:07 +0000

    Adapted metadata loading utils for new tws-player

--- src/util/metadataLoadingResult.ts
+++ src/util/metadataLoadingResult.ts
@@ -1,9 +1,10 @@
-import { FilePlayerSourceResolver } from "@app/domain/managers/resolver"
 import {
 	DefaultMetadataLoader,
 	MetadataLoader,
 	MetadataLoadingResult,
 	MetadataLoadingResultType,
+	PlayerSource,
+	StaticPlayerSource,
 } from "@teawithsand/tws-player"
 
 // TODO(teawithsand): move this to tws-player, as it's right place for such function
@@ -14,12 +15,12 @@ import {
  *
  * It never throws.
  */
-export const loadMetadataToResultHack = async <T>(
-	loader: MetadataLoader<T>,
-	entry: T
+export const loadMetadataToResultHack = async (
+	loader: MetadataLoader,
+	source: PlayerSource
 ): Promise<MetadataLoadingResult> => {
 	try {
-		const metadata = await loader.loadMetadata(entry)
+		const metadata = await loader.loadMetadata(source)
 		return {
 			type: MetadataLoadingResultType.OK,
 			metadata,
@@ -35,11 +36,12 @@ export const loadMetadataToResultHack = async <T>(
 export const loadBlobMetadataUtil = async (
 	blob: Blob
 ): Promise<MetadataLoadingResult> => {
-	const metadataLoader = new DefaultMetadataLoader(
-		new FilePlayerSourceResolver()
-	)
+	const metadataLoader = new DefaultMetadataLoader()
 	// Please note that this has to be done OUTSIDE addInternalFile callback, as it must be synchronous/await only
 	// db promises
-	const metadata = await loadMetadataToResultHack(metadataLoader, blob)
+	const metadata = await loadMetadataToResultHack(
+		metadataLoader,
+		new StaticPlayerSource(blob)
+	)
 	return metadata
 }
