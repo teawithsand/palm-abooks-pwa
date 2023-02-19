d9dcef9426f2cb7d824dff80a709a3568cc32282
teawithsand <teawithsand@gmail.com>
2023-02-19 19:32:15 +0000

    Refactored PlayerEntry/List Added PlayerEntryBag type

--- src/domain/managers/newPlayer/list/metadata.ts
+++ src/domain/managers/newPlayer/list/metadata.ts
@@ -1,4 +1,6 @@
 import { AbookEntity } from "@app/domain/defines/entity/abook"
+import { PositionVariants } from "@app/domain/defines/position"
+import { TimestampMs, generateUUID } from "@teawithsand/tws-stl"
 
 export enum PlayerEntryListMetadataType {
 	ABOOK = 1,
@@ -9,14 +11,37 @@ export enum PlayerEntryListMetadataType {
 /**
  * Metadata attached to any kind of list.
  */
-export type PlayerEntryListMetadata =
+export type PlayerEntryListMetadataData =
 	| {
 			type: PlayerEntryListMetadataType.ABOOK
 			abook: AbookEntity
 	  }
 	| {
-			type: PlayerEntryListMetadataType.UNKNOWN
+			type: PlayerEntryListMetadataType.UNKNOWN // Anything else I guess
 	  }
 	| {
-			type: PlayerEntryListMetadataType.LOCAL_FILES // files are not included here
+			type: PlayerEntryListMetadataType.LOCAL_FILES // files are not included here; they are entries.
 	  }
+
+export class PlayerEntryListMetadata {
+	constructor(
+		public readonly data: PlayerEntryListMetadataData,
+		public readonly id = generateUUID()
+	) {}
+
+	get positionToLoad(): PositionVariants | null {
+		if (this.data.type === PlayerEntryListMetadataType.ABOOK) {
+			return this.data.abook.position?.variants ?? null
+		}
+
+		return null
+	}
+
+	get lastPlayedTimestamp(): TimestampMs | null {
+		if (this.data.type === PlayerEntryListMetadataType.ABOOK) {
+			return this.data.abook.position?.savedTimestamp ?? null
+		}
+
+		return null
+	}
+}
