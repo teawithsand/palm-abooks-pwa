9bd13fe209dcca59ec878ad63d0a97829ca5f75e
teawithsand <teawithsand@gmail.com>
2023-02-19 20:04:40 +0000

    Added new and shiny seekBack types

---
+++ src/domain/defines/player/seekBack/defines.ts
@@ -0,0 +1,29 @@
+
+export enum SeekBackStrategyDataType {
+	NONE = 0,
+	LINEAR = 1,
+	STEPS = 2,
+	CONSTANT = 3,
+}
+
+export type SeekBackStrategyData =
+	| {
+			type: SeekBackStrategyDataType.NONE
+	  }
+	| {
+			type: SeekBackStrategyDataType.LINEAR
+			coefficient: number
+			limit: number
+	  }
+	| {
+			type: SeekBackStrategyDataType.CONSTANT
+			value: number
+	  }
+	| {
+			type: SeekBackStrategyDataType.STEPS
+			steps: {
+				maxSleepTime: number
+				strategy: SeekBackStrategyData
+			}[]
+			fallback: SeekBackStrategyData
+	  }
