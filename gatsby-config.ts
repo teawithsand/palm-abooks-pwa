446de3deeaab459d3d4c6107817f8d6ddd51342a
teawithsand <teawithsand@gmail.com>
2023-02-24 22:03:46 +0000

    Updated gatsby-config and icon to make it PWA installable

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,16 +1,29 @@
 import {
 	customizeDefaultPlugins,
-	GatsbyTransformerRemarkPlugins,
 	makeConfig,
 	makeLayoutPlugin,
 	makeManifestPlugin,
 	makeSelfPlugin,
+	makeOfflinePlugin,
 } from "@teawithsand/tws-gatsby-plugin"
 
 const plugins = customizeDefaultPlugins(
 	[
-		makeManifestPlugin("./src/images/icon.png"),
+		makeManifestPlugin("./src/images/icon-prefix-magic-icon.png", {
+			cache_busting_mode: 'none',
+			lang: 'en',
+			name: `PalmABooks PWA`,
+			short_name: `PalmABooks`,
+			description: "It plays and stores user-provided ABooks.",
+			start_url: `/`,
+			background_color: `#f7f0eb`,
+			theme_color: `#a2466c`,
+			display: `standalone`,
+		}),
 		makeLayoutPlugin("./src/Layout.tsx"),
+		makeOfflinePlugin({
+			cacheId: "palm-abooks-pwa-app-cache"
+		})
 	],
 	[
 		makeSelfPlugin({
