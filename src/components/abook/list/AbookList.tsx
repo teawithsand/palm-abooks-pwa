ffb9441a490ea1344992636a8fb7d4be335c83f9
teawithsand <teawithsand@gmail.com>
2023-02-25 21:36:00 +0000

    Added no abook notice in abook list

--- src/components/abook/list/AbookList.tsx
+++ src/components/abook/list/AbookList.tsx
@@ -1,6 +1,9 @@
 import { AbookSmallCard } from "@app/components/abook/list/AbookSmallCard"
 import { AbookEntity } from "@app/domain/defines/entity/abook"
+import { useAppPaths } from "@app/paths"
+import { LinkContainer } from "@app/util/LinkContainer"
 import React from "react"
+import { Button } from "react-bootstrap"
 import styled from "styled-components"
 
 const List = styled.div`
@@ -9,7 +12,29 @@ const List = styled.div`
 	gap: 1em;
 `
 
+const NoAbooksContainer = styled.div`
+	display: grid;
+	gap: 1em;
+	grid-auto-flow: row;
+	text-align: center;
+`
+
 export const AbookList = (props: { abooks: AbookEntity[] }) => {
+	const { abookAddLocalPath } = useAppPaths()
+	if (!props.abooks.length) {
+		return (
+			<NoAbooksContainer>
+				<div>You haven't added any abooks yet.</div>
+				<div>
+					<LinkContainer to={abookAddLocalPath}>
+						<Button href="#">
+							Here you may add some from local device
+						</Button>
+					</LinkContainer>
+				</div>
+			</NoAbooksContainer>
+		)
+	}
 	return (
 		<List>
 			{props.abooks.map((v, i) => (
