1e8ab619a52adccac5df8f6bbb027d7dc1f948c7
teawithsand <teawithsand@gmail.com>
2023-02-25 21:47:54 +0000

    Added proper fallback cover for player/abook view

--- src/components/player/element/PlayerCoverDisplay.tsx
+++ src/components/player/element/PlayerCoverDisplay.tsx
@@ -1,31 +1,43 @@
 import { useAppManager } from "@app/domain/managers/app"
 import { useUiPlayerData } from "@app/domain/ui/player"
 import styled from "styled-components"
-import React from "react"
-
-const defaultCoverUrl = "http://placekitten.com/300/300"
+import React, { useMemo } from "react"
+import { DEFAULT_IMAGE_COVER_URL } from "@app/domain/ui/abook"
+import { useStickySubscribableSelector } from "@teawithsand/tws-stl-react"
+import { useFileEntryEntityUrl } from "@app/domain/defines/entity/fileEntryHook"
+import { FileEntryDisposition } from "@app/domain/defines/abookFile"
+import { usePlayerSourceUrl } from "@app/domain/defines/player/playerSourceHook"
 
 const Container = styled.div`
 	display: grid;
 	align-items: center;
 	justify-items: center;
-    overflow: hidden;
+	overflow: hidden;
 
-    > img {
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-    }
+	> img {
+		height: 100%;
+		width: 100%;
+		object-fit: contain;
+	}
 `
 
 export const PlayerCoverDisplay = () => {
-	const ui = useUiPlayerData()
+	const app = useAppManager()
+	const fullList = useStickySubscribableSelector(
+		app.playerManager.bus,
+		(state) => state.playerEntryListManagerState.states.full.entriesBag
+	)
+	const entry = useMemo(() => {
+		return fullList.findFirstEntryWithDisposition(
+			FileEntryDisposition.IMAGE
+		)
+	}, [fullList])
 
-    // TODO(teawithsand): implement this
+	const coverUrl = usePlayerSourceUrl(entry?.source ?? null)
 
 	return (
 		<Container>
-			<img src={defaultCoverUrl} />
+			<img src={coverUrl ?? DEFAULT_IMAGE_COVER_URL} />
 		</Container>
 	)
 }
---
+++ src/domain/defines/player/playerSourceHook.ts
@@ -0,0 +1,29 @@
+import { PlayerSource, PlayerSourceUrlClaim } from "@teawithsand/tws-player"
+import { useEffect, useState } from "react"
+
+export const usePlayerSourceUrl = (
+	source: PlayerSource | null
+): string | null => {
+	const [url, setUrl] = useState<string | null>(null)
+
+	useEffect(() => {
+		setUrl(null)
+
+		let isFreed = false
+		let claim: PlayerSourceUrlClaim | null = null
+		const p = async () => {
+			claim = (await source?.claimUrl()) ?? null
+			if (claim && !isFreed) {
+				setUrl(claim.url)
+			}
+		}
+
+		p()
+		return () => {
+			isFreed = true
+			claim?.close()
+		}
+	}, [source])
+
+    return url
+}
